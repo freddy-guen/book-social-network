b3fe35bb0ec9c18ca42ba58fde886dcb58801de3
Freddy <guenengafo-biroboy.f@mipih.fr>
2025-04-14 18:04:25 +0200

    Sécurité 6 : Finalisation du filtre Jwt (JwtFilter)

--- book-social-network-api/src/main/java/com/guen/book/security/SecurityConfig.java
+++ book-social-network-api/src/main/java/com/guen/book/security/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig
 {
 
     private final AuthenticationProvider authenticationProvider;
-    private JwtFilter jwtAuthFilter;
+    private final JwtFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
@@ -49,5 +49,7 @@ public class SecurityConfig
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 }
