36363a7568834e679ce1b3f58cfbb570df1a8b61
Freddy <guenengafo-biroboy.f@mipih.fr>
2025-04-18 19:08:45 +0200

    Implémentation du service d'authentification (login)

---
+++ book-social-network-api/src/main/java/com/guen/book/auth/AuthenticationRequest.java
@@ -0,0 +1,25 @@
+package com.guen.book.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class AuthenticationRequest
+{
+    @Email(message = "Format d'email invalide")
+    @NotEmpty(message = "L'email est obligatoire")
+    @NotBlank(message = "L'email' est obligatoire")
+    private String email;
+
+    @NotEmpty(message = "Le mot de passe est obligatoire")
+    @NotBlank(message = "Le mot de passe est obligatoire")
+    @Size(min = 8, message = "Le mot de passe doit contenir au moins 8 caractères")
+    private String password;
+}
--- book-social-network-api/src/main/java/com/guen/book/security/JwtService.java
+++ book-social-network-api/src/main/java/com/guen/book/security/JwtService.java
@@ -55,7 +55,7 @@ public class JwtService
                 .getBody();
     }
 
-    private String generateToken(HashMap<String, Object> claims, UserDetails userDetails)
+    public String generateToken(HashMap<String, Object> claims, UserDetails userDetails)
     {
         return buildToken(claims, userDetails, jwtExpiration);
     }
