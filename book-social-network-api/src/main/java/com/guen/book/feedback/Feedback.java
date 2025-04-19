a4b26d411940f0f14da486833922ab3330f335a9
Freddy <guenengafo-biroboy.f@mipih.fr>
2025-04-19 11:16:16 +0200

    Héritage et refactoring

--- book-social-network-api/src/main/java/com/guen/book/feedback/Feedback.java
+++ book-social-network-api/src/main/java/com/guen/book/feedback/Feedback.java
@@ -1,45 +1,25 @@
 package com.guen.book.feedback;
 
-import jakarta.persistence.*;
-import lombok.*;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
+import com.guen.book.common.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@Builder
+@SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @EntityListeners(AuditingEntityListener.class)
-public class Feedback
+public class Feedback extends BaseEntity
 {
-    @Id
-    @GeneratedValue
-    private Integer id;
-
     private Double note; // 1 à 5 (étoiles)
 
     private String comment;
-
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdDate;
-
-    @LastModifiedDate
-    @Column(insertable = false)
-    private LocalDateTime lastModifiedDate;
-
-    @CreatedBy
-    @Column(nullable = false, updatable = false)
-    private Integer createdBy;
-
-    @LastModifiedBy
-    @Column(insertable = false)
-    private Integer lastModifiedBy;
 }
