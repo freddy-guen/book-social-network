7185cac50dd0138ad8ae8184447ade1f564b7753
Freddy <guenengafo-biroboy.f@mipih.fr>
2025-04-19 11:05:47 +0200

    Création des entités Book et FeedBack

---
+++ book-social-network-api/src/main/java/com/guen/book/feedback/Feedback.java
@@ -0,0 +1,45 @@
+package com.guen.book.feedback;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Feedback
+{
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private Double note; // 1 à 5 (étoiles)
+
+    private String comment;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+
+    @CreatedBy
+    @Column(nullable = false, updatable = false)
+    private Integer createdBy;
+
+    @LastModifiedBy
+    @Column(insertable = false)
+    private Integer lastModifiedBy;
+}
