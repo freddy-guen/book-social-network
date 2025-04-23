e6bea2aeda82254b8d063461e97f92afd204428f
Freddy <guenengafo-biroboy.f@mipih.fr>
2025-04-23 12:11:17 +0200

    Documentation OpenApi

---
+++ book-social-network-api/src/main/java/com/guen/book/config/OpenApiConfig.java
@@ -0,0 +1,55 @@
+package com.guen.book.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Guenengafo",
+                        email = "contact@guenengafo.fr",
+                        url = "https://guenengafo.fr"
+                ),
+                description = "Documentation OpenApi pour Spring security",
+                title = "Spécification OpenApi - Guenengafo",
+                version = "1.0",
+                license = @License(
+                        name = "Nom de la licence",
+                        url = "https://guenengafo.fr/licences"
+                ),
+                termsOfService = "Conditions d'utilisation"
+        ),
+        servers = {
+                @Server(
+                        description = "Environnement de développement : Local",
+                        url = "http://localhost:8088/api/v1"
+                ),
+                @Server(
+                        description = "Environnement de production : Prod",
+                        url = "https://guenengafo.fr/projets"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig
+{
+}
