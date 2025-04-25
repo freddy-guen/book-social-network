17148db72fb39e10069a26e8286b28cfb5c7db80
Freddy <guenengafo-biroboy.f@mipih.fr>
2025-04-25 17:12:54 +0200

    Implémentation du composant menu

--- book-social-network-ui/src/app/modules/book/pages/main/main.component.ts
+++ book-social-network-ui/src/app/modules/book/pages/main/main.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {MenuComponent} from '../../components/menu/menu.component';
 
 @Component({
   selector: 'app-main',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    MenuComponent
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
