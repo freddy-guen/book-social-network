32c69c26e6ee557c1488da19eabfdf58b15d49ae
Freddy <guenengafo-biroboy.f@mipih.fr>
2025-04-27 11:55:38 +0200

    Ajout des actions (sans les traiter)

--- book-social-network-ui/src/app/modules/book/components/book-card/book-card.component.ts
+++ book-social-network-ui/src/app/modules/book/components/book-card/book-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {BookResponse} from '../../../../services/models/book-response';
 import {NgIf} from '@angular/common';
 
@@ -44,6 +44,48 @@ export class BookCardComponent {
   @Input()
   set manage(value: boolean) {
     this._manage = value;
+  }
+
+  @Output()
+  private details: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+
+  @Output()
+  private borrow: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+
+  @Output()
+  private addToWaitingList: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+
+  @Output()
+  private edit: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+
+  @Output()
+  private share: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+
+  @Output()
+  private archive: EventEmitter<BookResponse> = new EventEmitter<BookResponse>();
+
+  onShowDetails() {
+    this.details.emit(this._book);
+  }
+
+  onBorrow() {
+    this.borrow.emit(this._book);
+  }
+
+  onAddToWaitingList() {
+    this.addToWaitingList.emit(this._book);
+  }
+
+  onEdit() {
+    this.edit.emit(this._book);
+  }
+
+  onShare() {
+    this.share.emit(this._book);
+  }
+
+  onArchive() {
+    this.archive.emit(this._book);
   }
 
 }
